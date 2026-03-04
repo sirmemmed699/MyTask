75236059e6b887cb5e138e43381d5ef44bb6abb5
sirmemmed699 <mmnbvcz089@gmail.com>
2026-03-04 10:42:10 +0400

    Calculiator Full

--- example.js
+++ example.js
@@ -8,3 +8,13 @@ const creditOfCount = 5;
 const creditOfAmount = parseFloat((totalAmount / 60)*creditOfCount)/4;
 
 console.log(creditOfAmount);
+
+
+// write same code as a function
+
+function calculiatedCreditOfAmount(totalAmount, creditOfCount){
+    return((totalAmount / 60)* creditOfCount)/4;
+}
+
+const creditOfAmount2 = calculiatedCreditOfAmount(totalAmount, creditOfCount);
+console.log(creditOfAmount2)
